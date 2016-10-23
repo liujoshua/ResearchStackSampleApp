90ff2be04f37b8acf512b26896c1a452392dece7
Joshua Liu <joshua.liu@sagebase.org>
2016-10-23 12:19:22 -0700

    Refactor to use new Bridge library

--- bridge-android-sdk/src/main/java/org/sagebionetworks/bridge/sdk/rest/model/UploadSession.java
+++
@@ -1,9 +0,0 @@
-package org.sagebionetworks.bridge.sdk.rest.model;
-
-public class UploadSession
-{
-    public String id;
-    public String url;
-    public String expires;
-    public String type;
-}
